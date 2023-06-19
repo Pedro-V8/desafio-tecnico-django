723d314b168806a0e08c475595545390eb20d9b2
Pedro Vieira <c00123@comunix.br>
2023-06-19 14:35:07 -0300

    Finalizando Formulario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import React, { useEffect, useState } from 'react';
 
 function App() {
   const [data, setData] = useState(null);
+  const [formulario, setFormulario] = useState([]);
 
   useEffect(() => {
     fetchData();
@@ -14,30 +15,58 @@ function App() {
   const fetchData = async () => {
     try {
       const response = await axios.get('http://localhost:8000/formulario/1/');
-      console.log(response.data)
       setData(response.data);
     } catch (error) {
       console.log(error);
     }
   };
 
+  const handleFieldChange = (event, campoId) => {
+    const valor = event.target.value;
+    setFormulario((prevFormulario) => ({
+      ...prevFormulario,
+      [campoId]: { id_formulario: data.id, id_campo: campoId, valor },
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    
+
+    let arr = []
+
+    for (let key in formulario) {
+        arr.push(formulario[key])
+    }
+
+    try {
+        const response = await axios.post('http://localhost:8000/registro/', arr);
+        console.log(response.data);
+        // Faça algo com a resposta, se necessário
+
+        alert("Registrado com Sucesso!")
+      } catch (error) {
+        console.log(error);
+        // Trate o erro, se necessário
+      }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      {data &&
+        data.campos.map((item) => (
+          <div key={item.id}>
+            <label htmlFor={item.nome_campo}>{item.nome_campo}:</label>
+            <input
+              type="text"
+              id={item.nome_campo}
+              value={formulario[item.id]?.valor || ''}
+              onChange={(event) => handleFieldChange(event, item.id)}
+            />
+          </div>
+        ))}
+      <button type="submit">Enviar</button>
+    </form>
   );
 }
 
